31bda6ae32cc8d229200efa1bf409e241f748fb0
hous <304536797@qq.com>
2018-05-09 21:29:33 +0800

    update getignore

--- run_appim.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -d --name appium -p 4723:4723 --privileged -v /dev/bus/usb:/dev/bus/usb appium/appium
--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-docker run --privileged -d \
--p 6080:6080 -p 5554:5554 -p 5555:5555 -p 4723:4723 \
--e DEVICE = "Samsung Galaxy S6" \
--e APPIUM = True \
---name android-container \
-butomo1989/docker-Android-x86-7.1.1
